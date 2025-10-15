94b6b8755a8e421b8ec36cd927aacb9ce048ec23
Jasleen74 <preetjee@gmail.com>
2025-10-15 19:41:18 +0530

    // if we want it to give us sometehing we need to write this  //void display() { //printf("hi i am display\n"); // Printing the message //}

--- void.c
+++ void.c
@@ -8,3 +8,7 @@
    
   return 0; // Here it's won't give anything only errors 🙄
  }
+// if we want it to give us sometehing we need to write this
+ //void display() {
+//printf("hi i am display\n"); // Printing the message
+//}
